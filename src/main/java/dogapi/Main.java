4c542d2c25ffab1e9215cce29e3edd408a2a0ad0
shiraz42 <shirazlarik42@gmail.com>
2025-10-27 20:09:03 -0400

    task3-complete

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -24,8 +24,12 @@ public class Main {
      * returned by the fetcher
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-        // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
-        // return statement included so that the starter code can compile and run.
-        return -1;
+        try {
+            List<String> subBreeds = breedFetcher.getSubBreeds(breed);
+            return subBreeds.size(); // returns 0 if list is empty
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            // breed not found, so return 0 as per instructions
+            return 0;
+        }
     }
 }
